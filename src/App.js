54891dd89e6d299b7ec533c62aadc5fa5d7b9493
shubhamdangi <shubham.d3v@gmail.com>
2022-01-14 11:26:45 +0530

    refactored feed and LF

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import { About, Home, Premium, Contact, Faq, Careers, Share } from "./pages/";
 import { UserContextProvider } from "./contexts/user";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Footer,ScrollToTop, Navbar1, Publish } from "./containers";
+import { Footer, ScrollToTop, Navbar1, Publish } from "./containers";
 import ReqPremium from "./pages/reqPremium";
-// Google Analytics id: UA-211293240-1
 import ReactGA from "react-ga";
 import Lostfound from "./containers/Lost-Found";
-ReactGA.initialize("UA-211293240-1");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
@@ -16,42 +14,42 @@ function App() {
     <UserContextProvider>
       <div className="App">
         <Router>
-        <ScrollToTop>
-          <Navbar1 />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/premium">
-              <Premium />
-            </Route>
-            <Route exact path="/premium/apply">
-              <ReqPremium />
-            </Route>
-            <Route exact path="/publish">
-              <Publish />
-            </Route>
-            <Route exact path="/about">
-              <About />
-            </Route>
-            <Route exact path="/contact">
-              <Contact />
-            </Route>
-            <Route exact path="/faq">
-              <Faq />
-            </Route>
-            <Route exact path="/careers">
-              <Careers />
-            </Route>
-            <Route exact path="/lostfound">
-              <Lostfound />
-            </Route>
-            <Route exact path="/share">
-              <Share />
-            </Route>
-          </Switch>
-        <Footer />
-        </ScrollToTop>
+          <ScrollToTop>
+            <Navbar1 />
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/premium">
+                <Premium />
+              </Route>
+              <Route exact path="/premium/apply">
+                <ReqPremium />
+              </Route>
+              <Route exact path="/publish">
+                <Publish />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/contact">
+                <Contact />
+              </Route>
+              <Route exact path="/faq">
+                <Faq />
+              </Route>
+              <Route exact path="/careers">
+                <Careers />
+              </Route>
+              <Route exact path="/lostfound">
+                <Lostfound />
+              </Route>
+              <Route exact path="/share">
+                <Share />
+              </Route>
+            </Switch>
+            <Footer />
+          </ScrollToTop>
         </Router>
       </div>
     </UserContextProvider>
